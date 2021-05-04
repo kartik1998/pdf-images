5641dad53b2607e3cc45db50395683aea3638a7b
Kartik <kartik@hyperverge.co>
2021-05-04 16:05:53 +0530

    Update: Export Poppler and ImageMagick classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import Poppler from './pdf-images/poppler';
 import ImageMagick from './pdf-images/image-magick';
 
-const out = ImageMagick.convert(
-  '/Users/kartik/Desktop/projects/pdf-images/test/sample.pdf',
-  '/Users/kartik/Desktop/projects/pdf-images/test/',
-  'out',
-);
-
-console.log(out);
-ImageMagick.setQuality(50);
-ImageMagick.setDensity(30);
-console.log(ImageMagick.getSettings());
+export { 
+  Poppler,
+  ImageMagick
+}
