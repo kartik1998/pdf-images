4b5e7e1ec28082f306fac593aedc399e548c5ae4
Kartik <kartik@hyperverge.co>
2021-05-04 16:41:25 +0530

    Fix: build directory

--- build/index.js
+++ build/index.js
@@ -1,17 +1,10 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const image_magick_1 = __importDefault(require('./pdf-images/image-magick'));
-const out = image_magick_1.default.convert(
-  '/Users/kartik/Desktop/projects/pdf-images/test/sample.pdf',
-  '/Users/kartik/Desktop/projects/pdf-images/test/',
-  'out',
-);
-console.log(out);
-image_magick_1.default.setQuality(50);
-image_magick_1.default.setDensity(30);
-console.log(image_magick_1.default.getSettings());
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ImageMagick = exports.Poppler = void 0;
+const poppler_1 = __importDefault(require("./pdf-images/poppler"));
+exports.Poppler = poppler_1.default;
+const image_magick_1 = __importDefault(require("./pdf-images/image-magick"));
+exports.ImageMagick = image_magick_1.default;
--- build/pdf-images/image-magick.js
+++ build/pdf-images/image-magick.js
@@ -1,61 +1,60 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const { execFileSync } = require('child_process');
-const fs_1 = __importDefault(require('fs'));
-const path_1 = __importDefault(require('path'));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 /**
  * @class {Poppler}
  * @description extracts all the images from a pdf and writes them to a directory
  * @performance takes more time than poppler but more reliable in conversions
  * */
 class ImageMagick {
-  /**
-   * @param pdfPath Path of the pdf that you want to extract images from
-   * @param outputImgDir The output image directory
-   * @param outputImgName The prefix image name of all the images extracted. eg: outputImgDir/outputImgName/outputImgName-001.png
-   * @returns {infoObject} If successfully converted infoObject contains the outputImgDirectoru and an array of image paths else it has an error object.
-   */
-  static convert(pdfPath, outputImgDir, outputImgName) {
-    const outputImgPath = path_1.default.join(outputImgDir, outputImgName);
-    if (!fs_1.default.existsSync(outputImgPath)) {
-      fs_1.default.mkdirSync(outputImgPath);
+    /**
+     * @param pdfPath Path of the pdf that you want to extract images from
+     * @param outputImgDir The output image directory
+     * @param outputImgName The prefix image name of all the images extracted. eg: outputImgDir/outputImgName/outputImgName-001.png
+     * @returns {infoObject} If successfully converted infoObject contains the outputImgDirectoru and an array of image paths else it has an error object.
+     */
+    static convert(pdfPath, outputImgDir, outputImgName) {
+        const outputImgPath = path_1.default.join(outputImgDir, outputImgName);
+        if (!fs_1.default.existsSync(outputImgPath)) {
+            fs_1.default.mkdirSync(outputImgPath);
+        }
+        const infoObject = { pdfPath };
+        try {
+            execFileSync('convert', [
+                '-quiet',
+                '-density',
+                this.density,
+                pdfPath,
+                '-quality',
+                this.quality,
+                outputImgPath + '/' + outputImgName + '.png',
+            ]);
+            infoObject.outputImagesDirectory = outputImgPath;
+            infoObject.images = fs_1.default.readdirSync(outputImgPath).map((img) => outputImgPath + '/' + img);
+            infoObject.success = true;
+        }
+        catch (err) {
+            infoObject.error = err;
+        }
+        return infoObject;
     }
-    const infoObject = { pdfPath };
-    try {
-      execFileSync('convert', [
-        '-quiet',
-        '-density',
-        this.density,
-        pdfPath,
-        '-quality',
-        this.quality,
-        outputImgPath + '/' + outputImgName + '.png',
-      ]);
-      infoObject.outputImagesDirectory = outputImgPath;
-      infoObject.images = fs_1.default.readdirSync(outputImgPath).map((img) => outputImgPath + '/' + img);
-      infoObject.success = true;
-    } catch (err) {
-      infoObject.error = err;
+    static setQuality(quality) {
+        this.quality = quality;
     }
-    return infoObject;
-  }
-  static setQuality(quality) {
-    this.quality = quality;
-  }
-  static setDensity(density) {
-    this.density = density;
-  }
-  static getSettings() {
-    const out = {};
-    out.quality = this.quality;
-    out.density = this.density;
-    return out;
-  }
+    static setDensity(density) {
+        this.density = density;
+    }
+    static getSettings() {
+        const out = {};
+        out.quality = this.quality;
+        out.density = this.density;
+        return out;
+    }
 }
 exports.default = ImageMagick;
 ImageMagick.quality = 100;
